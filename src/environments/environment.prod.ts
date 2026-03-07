ca93e7a84206f7fcb81690d5ee628c2fe8a104da
brolol <anshajshaji3@gmail.com>
2026-03-07 19:06:13 +0530

    resolved api issues hopefully

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://onyvmppqjazbokqrdszx.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ueXZtcHBxamF6Ym9rcXJkc3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MzY5ODYsImV4cCI6MjA4ODExMjk4Nn0.3TMO78-m5pnt68mwmFNcr1hfqQkPzXvXdWC8dJb4qQQ',
-  geminiApiKey: 'AIzaSyAi7n_2H5PpgVHm0rPdEXEWh-KZoul2jOA' // Replace with your actual key
+  geminiApiKey: 'AIzaSyBKR4ki4BCHcRqD_EmRTISBxoPKxcMCETw' // Replace with your actual key
 };
